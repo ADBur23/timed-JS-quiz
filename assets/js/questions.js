5e755f45908df676a992f3478441cd5e67662975
ADBur23 <antburgess289@gmail.com>
2023-12-04 18:52:28 +0000

    added questions to quetions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+// Questions
+
+var questions = [
+    {
+      question: "Question: What is the HTML tag under which you can write the JavaScript code?",
+      answerChoice: [
+        "A) <javascript>",
+        "B) <scripted>",
+        "C) <script>",
+        "D) <js>",
+      ],
+      correctAnswer: 2,
+    },
+    {
+      question: "Question: What are variables used for in JavaScript Programs?",
+      answerChoice: [
+        "A) Storing numbers, dates, or other values",
+        "B) Varying randomly",
+        "D) Causing high-school algebra flashbacks",
+        "D) None of the above",
+      ],
+      correctAnswer: 0,
+    },
+    {
+      question:"Question: Which method adds a new item to the end of an array and returns the new length?",
+      answerChoice: ["A) shift()", "B) return() ", "C) push() ", "D) pop()"],
+      correctAnswer: 2,
+    },
+    {
+      question:"Question: Which of the following can't be done with client-side JavaScript?",
+      answerChoice: [
+        "A) Sending a form's contents by email",
+        "B) Validating a form",
+        "C) Storing the form's contents to a database file on the server",
+        "D) None of the above",
+      ],
+      correctAnswer: 2,
+    },
+    {
+      question:"Question: Which of the following are capabilities of functions in JavaScript?",
+      answerChoice: [
+        "A) Return a value",
+        "B) Accept parameters",
+        "C) Accept parameters and Return a value",
+        "D) All of the above",
+      ],
+      correctAnswer: 1,
+    },
+  ];
